8b8b2fd247e8e2719fa52cd949c214ebf86c165b
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-03-04 11:11:01 +0100

    renamed class name and added date time exercises of 1,2,3 using localdate, dateformatter in java

---
+++ src/main/java/lexicon/exercises/DateTimeExercise.java
@@ -0,0 +1,41 @@
+package lexicon.exercises;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateTimeExercise {
+    
+    static void main() {
+        IO.println("Hello and welcome to Java fundamentals_2 Exercises !!!!");
+        exercise1();
+        exercise2();
+        exercise3();
+    }
+
+    //Exercise 1 – Current Date
+    //Create a LocalDate of the current day and print it out.
+    static void exercise1(){
+        LocalDate localDate = LocalDate.now();
+        IO.println("Current Local date is : "+localDate);
+    }
+
+    //Exercise 2 – Formatted Current Date
+    //Create a LocalDate of the current day and print it out in the following pattern using `DateTimeFormatter`: **Torsdag 29 mars**
+    static void exercise2(){
+        LocalDate localDate = LocalDate.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("EEEE d MMMM", new Locale("sv", "SE"));
+        String date = localDate.format(dateTimeFormatter);
+        IO.println("Current Local date using DateTimeFormatter is : "+date);
+    }
+
+    //Exercise 3 – Last Monday and the Entire Week
+    //Create a LocalDate of last Monday. Then print out the entire week in a loop using standard ISO format.
+    static void exercise3(){
+        LocalDate localDate = LocalDate.of(2026,02,23);
+        for(int i = 1; i < 7 ; i++){
+            LocalDate date = localDate.plusDays(i-1);
+            IO.println("Day "+ (i + 1) + " of last week is : "+date);
+        }
+    }
+}
